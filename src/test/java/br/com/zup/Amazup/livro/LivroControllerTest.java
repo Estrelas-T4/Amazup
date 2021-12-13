537d9685427e03a2abd2e5dffd7ff353751d25fa
Vinícius <vinicius.raymundo@zup.com.br>
2021-12-13 16:37:24 -0300

    Implementar teste para cadastro de livro

--- src/test/java/br/com/zup/Amazup/livro/LivroControllerTest.java
+++ src/test/java/br/com/zup/Amazup/livro/LivroControllerTest.java
@@ -4,10 +4,16 @@ import br.com.zup.Amazup.autor.Autor;
 import br.com.zup.Amazup.enums.Genero;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @WebMvcTest(LivroController.class)
 public class LivroControllerTest {
@@ -28,7 +34,7 @@ public class LivroControllerTest {
         livro = new Livro();
         livro.setGenero(Genero.FICCAO_CIENTIFICA);
         livro.setNome("Adeus e Obrigado Pelos Peixes");
-        livro.setId(1);
+        livro.setId(100000);
 
         autor = new Autor();
         autor.setId(1);
@@ -37,4 +43,17 @@ public class LivroControllerTest {
         livro.setAutor(autor);
     }
 
+    @Test
+    public void testarCadastroDeLivro() throws Exception{
+        Mockito.when(livroService.salvarLivro(Mockito.any(Livro.class))).thenReturn(livro);
+        String json = objectMapper.writeValueAsString(livro);
+
+        ResultActions resultadoDaRequisicao = mockMvc.perform(MockMvcRequestBuilders.post("/livros")
+                .contentType(MediaType.APPLICATION_JSON).content(json))
+                .andExpect(MockMvcResultMatchers.status().is(201))
+                .andExpect(
+                        MockMvcResultMatchers.jsonPath("$.vitrine")
+                                .value("http://localhost:8080/livros/"+livro.getId())
+                );
+    }
 }
