c687c364e5f45b2e66983d3a3dbeffdbb7627bf5
Vinícius <vinicius.raymundo@zup.com.br>
2021-12-13 15:25:23 -0300

    Criar entidade de autor

---
+++ src/main/java/br/com/zup/Amazup/autor/Autor.java
@@ -0,0 +1,33 @@
+package br.com.zup.Amazup.autor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Autor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nome;
+
+    public Autor() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
