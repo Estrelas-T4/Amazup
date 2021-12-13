bb3d85299c5455fe081445363e2524b931b59e6f
Vinícius <vinicius.raymundo@zup.com.br>
2021-12-13 15:23:13 -0300

    Criar plojeto

---
+++ src/main/java/br/com/zup/Amazup/AmazupApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.Amazup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/Amazup/AmazupApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.Amazup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
