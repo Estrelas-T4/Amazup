b200be1cd808bd2443bf5b92c3e954a53e8f57fe
Vinícius <vinicius.raymundo@zup.com.br>
2021-12-15 14:50:10 -0300

    Implementar validacao de livro já cadastro

--- src/main/java/br/com/zup/Amazup/livro/LivroService.java
+++ src/main/java/br/com/zup/Amazup/livro/LivroService.java
@@ -1,5 +1,6 @@
 package br.com.zup.Amazup.livro;
 
+import br.com.zup.Amazup.livro.exceptions.LivroJaCadastradoException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,14 +11,18 @@ public class LivroService {
     @Autowired
     private LivroRepository livroRepository;
 
-    public Livro salvarLivro(Livro livro){
-       Livro livroObjeto = livroRepository.save(livro);
-       return livroObjeto;
+    public Livro salvarLivro(Livro livro) {
+        if (livroRepository.existsByNomeAndAutorId(livro.getNome(), livro.getAutor().getId())) {
+            throw new LivroJaCadastradoException();
+        }
+        Livro livroObjeto = livroRepository.save(livro);
+
+        return livroObjeto;
     }
 
-    public Livro buscarLivroPorId(int id){
+    public Livro buscarLivroPorId(int id) {
         Optional<Livro> livroOptional = livroRepository.findById(id);
-        if(livroOptional.isEmpty()){
+        if (livroOptional.isEmpty()) {
             throw new RuntimeException();
         }
 
